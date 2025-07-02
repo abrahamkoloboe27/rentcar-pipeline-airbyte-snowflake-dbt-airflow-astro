61d90b64e87750f1310e61c263a378dac5371f8e
Abraham KOLOBOE <abrahamkoloboe2000@gmail.com>
2025-07-02 17:07:59 +0100

    refactor: move generate_data module to src directory
    Restructure project by moving the generate_data module and its dependencies to a src directory for better project organization and maintainability

--- dags/generate_data_dag.py
+++ dags/generate_data_dag.py
@@ -1,6 +1,6 @@
-from generate_data.create_index_mongo import generate_index
-from generate_data.create_data_mongo import generate_data_all
-from datetime import timedelta, datetime
+from src.generate_data.create_index_mongo import generate_index
+from src.generate_data.create_data_mongo import generate_data_all
+from datetime import timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
