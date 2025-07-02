5dd6bbe51bf2b9b025dced4fbca02ab6e90054b3
Abraham KOLOBOE <abrahamkoloboe2000@gmail.com>
2025-07-02 10:37:25 +0100

    chore: remove rentcar dbt project files and dependencies

--- dags/dbt/rentcar/rentcar/rentcar/resources.py
+++
@@ -1,9 +0,0 @@
-# resources.py
-import dagster as dg
-from dagster_airbyte import AirbyteCloudWorkspace
-
-airbyte_workspace = AirbyteCloudWorkspace(
-    workspace_id=dg.EnvVar("AIRBYTE_CLOUD_WORKSPACE_ID"),
-    client_id=dg.EnvVar("AIRBYTE_CLOUD_CLIENT_ID"),
-    client_secret=dg.EnvVar("AIRBYTE_CLOUD_CLIENT_SECRET"),
-)
